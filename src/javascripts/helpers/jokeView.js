c0c3b2f238a635b44a2349e8f1dd6f5aa23e21aa
Ryan McNair <ryan.mcnair@gmail.com>
2020-10-31 13:57:28 -0500

    fixed button issue

--- src/javascripts/helpers/jokeView.js
+++ src/javascripts/helpers/jokeView.js
@@ -2,13 +2,13 @@ import joke from '../components/joke';
 
 const jokeSetupView = () => {
   joke.jokeSetup().then((response) => {
-    $('#app').append(`<h2>${response}</h2><button id="punchline">?<button>`);
+    $('#app').append(`<h2>${response}</h2><button id="punchline">?</button>`);
   });
 };
 
 const jokePunchlineView = () => {
   joke.jokePunchline().then((response) => {
-    $('#app').append(`<h2>${response}</h2><button id="reload">ANOTHER?<button>`);
+    $('#app').append(`<h2>${response}</h2><button id="reload">ANOTHER?</button>`);
   });
 };
 
